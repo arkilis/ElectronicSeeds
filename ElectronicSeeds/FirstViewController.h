40e1ca931d90bc5103441061741b9d96ef909f75
Ben LIU <arkilis@gmail.com>
2015-09-18 07:02:21 +1000

    Initial Commit

---
+++ ElectronicSeeds/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  ElectronicSeeds
+//
+//  Created by Ben Liu on 18/09/2015.
+//  Copyright (c) 2015 Ben Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ ElectronicSeeds/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  ElectronicSeeds
+//
+//  Created by Ben Liu on 18/09/2015.
+//  Copyright (c) 2015 Ben Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
